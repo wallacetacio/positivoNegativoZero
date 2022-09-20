04bf6c2fdeea40a45d53e57bc29368a605b6dab5
Wallace Tácio <wallacett.dev@gmail.com>
2022-09-20 11:30:12 -0300

    Adiciona solução ao exercício Positivo, Negativo ou Zero FATEC 2022/2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite um número: ");
+
+Double num = Convert.ToDouble(Console.ReadLine());
+
+if(num > 0){
+    Console.WriteLine($"O número {num} é positivo!");
+}else if(num < 0){
+    Console.WriteLine($"O número {num} é negativo!");
+}else {
+    Console.WriteLine($"O número {num} é zero!");
+}
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # positivoNegativoZero
 Exercício Positivo Negativo ou Zero FATEC 2022/2
+
+Criar um programa que permita que o usuário digite um número, verificar e mostrar na tela se o número é positivo, negativo ou zero.
